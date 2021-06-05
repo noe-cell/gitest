d3d44fe28ee188ebbe25188c2834a0b92a41789e
15438 <1543836421@qq.com>
2021-06-05 16:08:15 +0800

    gitee commit

--- src/main/java/com/example/quan.java
+++ src/main/java/com/example/quan.java
@@ -9,7 +9,7 @@ public class quan {
         System.out.println("master test");
         System.out.println("hot-fix tesst");
         System.out.println("push test");
-          System.out.println("push test");
+          System.out.println("gitee test");
 
 
     }
